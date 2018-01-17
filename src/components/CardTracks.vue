3bf219036ed9258c4e3edfe9a50ea7bcc56ffc62
Ricardo Medina <rcrdmedina6@gmail.com>
2018-01-17 00:23:56 -0430

    Se modifico buscador para realizar busquedas a media que se escribe en el input, Se agrego loader al componente CardTracks para mostrar mientras se realiza la consulta a la API

--- src/components/CardTracks.vue
+++ src/components/CardTracks.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
-  article.card(@click="goToTrack(track)")
-    .card-image
-      figure.image.is-4by3
-        img(v-bind:src="track.image[2]['#text']", alt="Placeholder image")
-    .card-content
-      .media
-        .media-content
-          p.title {{ track.name }}
-          p.subtitle {{ track.artist.name ? track.artist.name : track.artist }}
-      .content
-        img(src="../assets/listen.png")
-        | {{ track.listeners ? track.listeners + ' listeners ' : "Popular right now." }}
+  article.card
+    .loader
+    .box-card-track(@click="goToTrack(track)")
+      .card-image
+        figure.image.is-4by3
+          img(v-bind:src="track.image[2]['#text']", alt="Placeholder image")
+      .card-content
+        .media
+          .media-content
+            p.title {{ track.name }}
+            p.subtitle {{ track.artist.name ? track.artist.name : track.artist }}
+        .content
+          img(src="../assets/listen.png")
+          | {{ track.listeners ? track.listeners + ' listeners ' : "Popular right now." }}
 </template>
 
 <script>
@@ -30,18 +32,20 @@ export default {
 
 <style lang="scss" scoped>
 
+@import 'src/scss/loader-card.scss';
+
 .card{
-  background-color: transparent;
+  background: rgba(0, 0, 0, 0.7);
   -ms-transition:all 0.6s ease-out;
   -moz-transition:all 0.6s ease-out;
   -o-transition:all 0.6s ease-out;
   -webkit-transition:all 0.6s ease-out;
   transition:all 0.6s ease-out;
+  min-height: 235px;
 }
 .card-content{
   padding: .2rem .9rem;
   height: 40px;
-  background: rgba(0, 0, 0, 0.7);
   color:rgb(222,89,34);
 }
 .card:hover{
@@ -52,6 +56,18 @@ export default {
   transition:all 0.6s ease-in;
   transform: scale(1.1);
 }
+.wrapper-card.is-loaded .card{
+  background: #de5a228a;
+  -ms-transition:all 0.6s ease-in;
+  -moz-transition:all 0.6s ease-in;
+  -o-transition:all 0.6s ease-in;
+  -webkit-transition:all 0.6s ease-in;
+  transition:all 0.6s ease-in;
+}
+.wrapper-card.is-loaded .card:hover{
+  transform: unset;
+  cursor:default;
+}
 .card-content .media{
   position: relative;
   top: -63px;
@@ -61,19 +77,20 @@ export default {
 }
 .media .media-content{
   height: 60px;
-  background: #de592257;
+  background: #de59229c;
   padding: .4rem 0 0 .5rem;
+  background: rgba(0, 0, 0, 0.7);
 }
 .card-content .content{
   position: relative;
-  top: -65px;
+  top: -76px;
 }
 .content img{
-  width: 20px;
+  width: 30px;
   display: inline;
   margin-right: 15px;
   position: relative;
-  top: 4px;
+  top: 9px;
 }
 .title{
   font-size: 1rem;
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,21 +5,25 @@
       .columns
         .column
           .container
-            nav.nav-search
-              .content-box  Track
-              form
-                input#boxSearch.search(
-                  type="text",
-                  autofocus,
-                  v-model='searchQuery',
-                  v-focus
-                )
-                a.button(@click="search")
-                  icon(name="question" scale="2")
-              .content-box Top geo
+            .wrapper-search
+              nav.nav-search
+                .content-box  Track
+                form
+                  input#boxSearch.search(
+                    type="text",
+                    autofocus,
+                    autocomplete="off",
+                    v-model='searchQuery',
+                    v-on:keyup="search",
+                    v-focus="true"
+                  )
+                  a.button
+                    icon(name="question" scale="2")
+                .content-box Top geo
+              .box-result(v-if="total > 0") {{ total }} results found
       .container
         .show-results
-          .wrapper-card(v-for="t in tracks")
+          .wrapper-card(v-for="t in tracks", v-bind:class="{ 'is-loaded': isLoadingTracks }")
              vm-card-tracks-popular(v-bind:track="t")
 </template>
 
@@ -37,12 +41,17 @@ export default {
     return {
       searchQuery: '',
       tracks: [],
-      isLoading: false
+      total: 0,
+      isLoading: false,
+      isLoadingTracks: false
     }
   },
   components: {
     VmCardTracksPopular,
     VmLoader
+  },
+  created () {
+    this.tracksPopular()
   },
   directives: {
     focus: {
@@ -51,12 +60,8 @@ export default {
       }
     }
   },
-  created () {
-    this.tracksPopular()
-  },
   methods: {
     tracksPopular () {
-      console.log('hola')
       trackService.geoGetTopTracks()
         .then(res => {
           this.tracks = res.tracks.track
@@ -64,10 +69,15 @@ export default {
         })
     },
     search () {
-      if (this.searchQuery === '') {}
+      if (this.searchQuery === '') return
+      this.isLoadingTracks = true
       trackService.search(this.searchQuery)
         .then(res => {
           this.tracks = res.results.trackmatches.track
+          this.total = this.tracks.length
+          setTimeout(() => {
+            this.isLoadingTracks = false
+          }, 3000)
         })
     }
   }
@@ -92,17 +102,23 @@ export default {
   margin-top: 2rem;
   cursor: pointer;
 }
+.wrapper-search{
+  background: #de5a22cc;
+  color: #fff;
+  text-align: center;
+  padding: 20px ;
+}
 .nav-search{
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 80px;
-  background: rgba(219, 88, 0, 0.774);
-  color: #fff;
   font-size: 2rem;
 }
 .content-box{
   margin: 0 .8rem;
+}
+.box-result{
+  margin-top:20px;
 }
 .search{
   height:50px;
@@ -121,14 +137,21 @@ export default {
   -webkit-transition:all 0.6s ease-out;
   transition:all 0.6s ease-out;
 }
-.search:focus{
-  background: rgba(255, 255, 255, 0.219);
+.search:focus,.wrapper-search:hover .search{
+  background: rgba(0, 0, 0, 0.7);
   -ms-transition:all 0.6s ease-in;
   -moz-transition:all 0.6s ease-in;
   -o-transition:all 0.6s ease-in;
   -webkit-transition:all 0.6s ease-in;
   transition:all 0.6s ease-in;
-  color: #fff;
+}
+.wrapper-search:hover .button{
+  color: rgba(0, 0, 0, 0.7);
+  -ms-transition:all 0.6s ease-in;
+  -moz-transition:all 0.6s ease-in;
+  -o-transition:all 0.6s ease-in;
+  -webkit-transition:all 0.6s ease-in;
+  transition:all 0.6s ease-in;
 }
 .button{
   border-radius: unset;
@@ -139,8 +162,13 @@ export default {
   height: 52px;
   top: -1px;
   width: 51px;
+  -ms-transition:all 0.6s ease-out;
+  -moz-transition:all 0.6s ease-out;
+  -o-transition:all 0.6s ease-out;
+  -webkit-transition:all 0.6s ease-out;
+  transition:all 0.6s ease-out;
 }
-.button:hover,.button.focus{
+.button:hover,.button:focus{
   border: unset; 
 }
 
